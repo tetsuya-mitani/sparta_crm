a5bed3a732194c6a4a1a9099a70136f496bbb950
三谷 哲也 <mitani0328@hotmail.co.jp>
2015-06-20 18:34:53 +0900

    検索機能の追加（ransack使用）

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,8 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :destroy, :update]
 
   def index
-    @customers = Customer.page(params[:page])
+    @q = Customer.ransack(params[:q])
+    @customers = @q.result.page(params[:page])
   end
 
   def new
