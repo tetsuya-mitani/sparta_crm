c54263b439bd0c7b733efeb221088a6d58c17d01
三谷 哲也 <mitani0328@hotmail.co.jp>
2015-06-20 10:59:45 +0900

    controller作成。dbやりなおし。view不要なもの削除。

--- db/migrate/20150620012125_create_customers.rb
+++ db/migrate/20150620012125_create_customers.rb
@@ -3,7 +3,7 @@ class CreateCustomers < ActiveRecord::Migration
     create_table :customers do |t|
       t.string :family_name,  null: false,  length: { maximum: 20 }
       t.string :given_name,   null: false,  length: { maximum: 20 }
-      t.string :email,        null: false,
+      t.string :email,        null: false
 
       t.timestamps null: false
     end
