ed28e903e532905c048c0a6a5123f076f9e1caa6
三谷 哲也 <mitani0328@hotmail.co.jp>
2015-06-21 11:11:14 +0900

    ユーザー認証追加。viewを追加。

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,6 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :destroy, :update]
+  before_action :authenticate_user!, only: [:edit, :destroy, :update]
 
   def index
     @q = Customer.ransack(params[:q])
